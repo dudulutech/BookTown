3d1764dab7981e182bca29ed585256ee243e3028
Zhel_bit <54180058+dudulutech@users.noreply.github.com>
2022-04-17 17:37:40 +0600

    Initial commit

---
+++ src/main/java/com/example/BookTown/BookTownApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookTown/BookTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
